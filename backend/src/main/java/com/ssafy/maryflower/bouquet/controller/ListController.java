a68350f96a7aa25c8e1aabe7de0d73d6996f2497
minahpark42 <minahpark42@gmail.com>
2024-03-28 15:41:16 +0900

    fixing connection

--- backend/src/main/java/com/ssafy/maryflower/bouquet/controller/ListController.java
+++ backend/src/main/java/com/ssafy/maryflower/bouquet/controller/ListController.java
@@ -2,30 +2,25 @@ package com.ssafy.maryflower.bouquet.controller;
 
 import com.ssafy.maryflower.bouquet.data.dto.request.BouquetListRequestDto;
 import com.ssafy.maryflower.bouquet.data.dto.response.BouquetFlowerResponseDto;
-import com.ssafy.maryflower.bouquet.data.repository.BouquetJpaRepository;
 import com.ssafy.maryflower.bouquet.service.ListService;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.web.PageableDefault;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
 @RestController
-@RequestMapping("/bouquet/list")
+@RequestMapping("/bouquet")
 @RequiredArgsConstructor
 @Slf4j
 public class ListController {
 
   private final ListService listService;
 
-  @GetMapping("")
-  public ResponseEntity<Slice<BouquetFlowerResponseDto>> search(BouquetListRequestDto req, @PageableDefault(size = 4) Pageable pageable) {
+  @GetMapping("/list")
+  public ResponseEntity<Slice<BouquetFlowerResponseDto>> search(@RequestBody BouquetListRequestDto req, @PageableDefault(size = 4) Pageable pageable) {
     Slice<BouquetFlowerResponseDto> res = listService.search(req, pageable);
     return ResponseEntity.ok(res);
   }
--- backend/src/test/java/com/ssafy/maryflower/MaryflowerApplicationTests.java
+++ backend/src/test/java/com/ssafy/maryflower/MaryflowerApplicationTests.java
@@ -1,16 +1,12 @@
 package com.ssafy.maryflower;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
-import com.ssafy.maryflower.bouquet.data.entity.Hello;
 import com.ssafy.maryflower.bouquet.data.entity.QHello;
 import jakarta.persistence.EntityManager;
 import jakarta.transaction.Transactional;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import static org.assertj.core.api.Assertions.*;
 
 @SpringBootTest
 @Transactional
